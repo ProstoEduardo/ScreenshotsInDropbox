3eec7e6c88a9fda87c0ee78c16df1a16182c8e64
eroshovedik <53685963+eroshovedik@users.noreply.github.com>
2020-01-24 21:31:44 +0300

    Update MyThread.java

--- src/MyThread.java
+++ src/MyThread.java
@@ -15,10 +15,10 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (;;) {
+        for (;;) {                                                  // Цикл неограничен,в дальнейшем возможны различные вариации
             System.out.println("Скрин отправлен в Dropbox");
             try {
-                sleep(5000);
+                sleep(5000);                                        // каждые 5 секунд
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -29,20 +29,20 @@ public class MyThread extends Thread {
             } catch (AWTException e) {
                 e.printStackTrace();
             }
-            BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));  // делаем скриншот экрана
 
-            String ACCESS_TOKEN = "vzmen4mMaPAAAAAAAAAAENGwZEzBvHxjxu-xAu_daO314ttdTiAKgvcFEvr8V7CK";
+            String ACCESS_TOKEN = "vzmen4mMaPAAAAAAAAAAENGwZEzBvHxjxu-xAu_daO314ttdTiAKgvcFEvr8V7CK";  // подключаем Dropbox к idea
             DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
             DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
 
             try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();           // перевод  BufferedImage в InputStream
                 ImageIO.write(screenShot, "png", baos);
                 InputStream is = new ByteArrayInputStream(baos.toByteArray());
 
-                SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
-                Date data = new Date();
-                FileMetadata metadata = client.files().uploadBuilder("/" + format.format(new Date()) + ".png")
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");           // задаем формат даты и времени
+                Date data = new Date();                                                     // опеределяем текущую дату и время
+                FileMetadata metadata = client.files().uploadBuilder("/" + format.format(new Date()) + ".png") // закидываем скриншот в Dropbox с названием в виде "ГГГГММЧЧ_ЧЧММСС"
                      .uploadAndFinish(is);
                 }
                 catch (Exception e){
